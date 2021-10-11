c6a781ffc3bb53cd02bb7a01257336f6c413ba7a
suman_achary <suman.achary95@gmail.com>
2021-10-11 21:40:12 +0530

    images_added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-
-const  App = () => {
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+const App = () => {
   return (
     <>
-       <h1>HELLO</h1>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <h1>lorem2</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
