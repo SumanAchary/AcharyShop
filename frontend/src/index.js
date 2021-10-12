c1953020e3aeaf0c62a08fb4a7bb84c44cf508d0
suman_achary <suman.achary95@gmail.com>
2021-10-12 23:29:14 +0530

    rreact_router_dom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,8 @@ import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <App />,
+
   document.getElementById("root")
 );
