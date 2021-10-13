1d61e9372793d0e8a94563e10ae3d65b87b8a4e6
suman_achary <suman.achary95@gmail.com>
2021-10-13 13:43:59 +0530

    front_end_done

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,34 @@
 import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 const Header = () => {
     return (
         <header>
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">Achary Store</Navbar.Brand>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand >Achary Store</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-                            <Nav.Link href="/cart"><i className="fas fa-cart-arrow-down"/> Cart</Nav.Link>
-                            <Nav.Link href="/login"><i className="fas fa-user" /> Sign In</Nav.Link>
+                            
+                            <LinkContainer to='/cart/'>
+                                <Nav.Link  >
+                                    <i className="fas fa-cart-arrow-down" /> Cart
+                                </Nav.Link>
+                            </LinkContainer>
+
+                            <LinkContainer to='/login/'>
+                                <Nav.Link>
+                                    <i className="fas fa-user" /> Sign In
+                                </Nav.Link>
+                            </LinkContainer>
+
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
-            </Navbar></header>
+            </Navbar></header >
     );
 };
 
