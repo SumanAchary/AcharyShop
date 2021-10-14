e6f9d37763eb5c25b109721a974cdb633bb16088
suman_achary <suman.achary95@gmail.com>
2021-10-14 20:32:54 +0530

    mongoDB__until Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,28 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import products from './data/products.js'
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import products from "./data/products.js";
+import colors from 'colors'
 
-dotenv.config()
-
+dotenv.config();
+connectDB()
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('API is runnng')
-})
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.get("/", (req, res) => {
+    res.send("API is runnng");
+});
+app.get("/api/products", (req, res) => {
+    res.json(products);
+});
 
-app.get('/api/product/:id', (req, res) => {
+app.get("/api/product/:id", (req, res) => {
     const product = products.find((p) => p._id === req.params.id);
-    res.json(product)
-})
+    res.json(product);
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Sever running on ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+    PORT,
+    console.log(`Sever running on ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
